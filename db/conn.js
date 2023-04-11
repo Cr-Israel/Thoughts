5d8deffd202036584def89d8003660684f33279c
Carlos Israel Mesquita Alvares <israel_alvares@Alvares.localdomain>
2023-04-11 11:14:36 -0300

    Deploy Vercel

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('database', 'user', 'password', {
+const sequelize = new Sequelize('teste', 'root', 'mesquita', {
     host: 'localhost',
     dialect: 'mysql'
 });
