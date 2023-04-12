0d2b83337c7b76a771f01402b64ea23aecb20307
Carlos Israel <98050243+Cr-Israel@users.noreply.github.com>
2023-04-12 09:33:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const flash = require('express-flash');
+const mysql2 = require('mysql2');
 
 const conn = require('./db/conn');
 
